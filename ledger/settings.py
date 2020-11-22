6ed02259da232ed27f564e261e9e0b187f2ac6d3
Brian Riviere <briviere@gmail.com>
2020-11-22 11:56:45 -0500

    Added Allowed Host

--- ledger/settings.py
+++ ledger/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'km&+&g_pcaj03g9dqhzg+qcg2)5bvu_rc*_n17!ol3571a8%u8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [diyaccounting.herokuapp.com]
 
 
 # Application definition
